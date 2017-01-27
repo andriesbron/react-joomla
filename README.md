aa4d1a7a167e5166a6827676faa6ead6b751544c
julien <julien.vonthron@gmail.com>
2017-01-27 17:46:18 +0100

    updated gulp script with copy/watch, and proper zip release

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-Install
+# Joomla React integration demo
+
+This is a very simple component for demonstrating the integration of React in a joomla extension.
+
+Build the zip using instructions, then install in backend, and create a menu item to see the app in frontend.
+
+## Build instructions
+Clone locally then run following commands
 ```
 npm install
 npm install @types/react
 sudo npm install tsd -g
 tsd reinstall
-gulp
+gulp release
 ```
+this should build the zip in the release folder.
+
+Optionally, you can edit gulp-config.dist.json and save as gulp-config.json to run `gulp` copy && watch to your local joomla install.
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,59 @@
-var gulp = require('gulp');
-var webpack = require('gulp-webpack');
+const gulp    = require('gulp');
+const webpack = require('gulp-webpack');
+const merge   = require('merge-stream');
+const del     = require('del');
+const zip     = require('gulp-zip');
 
-gulp.task('webpack', function() {
-    return gulp.src('')
-        .pipe(webpack( require('./webpack.config.js') ))
-        .pipe(gulp.dest('public/components/com_cars/assets/build'));
+const config = require('./gulp-config.json');
+
+const copyToJoomla = function() {
+	console.log('copy to ' + config.joomlaDir);
+
+	const copyStreams = [
+		gulp.src(['public/**', '!public/cars.xml', '!public/.gitignore'])
+			.pipe(gulp.dest(config.joomlaDir)),
+		gulp.src(['public/cars.xml'])
+			.pipe(gulp.dest(config.joomlaDir + '/administrator/components/com_cars'))
+	];
+
+	return merge(copyStreams);
+};
+
+// Check if config has defaultTasks defined
+const defaultTasks = config.hasOwnProperty('defaultTasks') ? config.defaultTasks : ['webpack'];
+gulp.task('default', defaultTasks);
+
+gulp.task('release', ['webpack'], function(){
+	const fileName = 'com_cars.zip';
+	const releaseDir = config.hasOwnProperty('releaseDir') ? config.releaseDir : './release';
+
+	gulp.src(['public/**'])
+		.pipe(zip(fileName))
+		.pipe(gulp.dest(releaseDir));
 });
 
-gulp.task('default', ['webpack']);
+gulp.task('webpack', function() {
+	return gulp.src('')
+		.pipe(webpack( require('./webpack.config.js') ))
+		.pipe(gulp.dest('public/components/com_cars/assets/build'));
+});
+
+// Watch
+gulp.task('watch', ['watch:react', 'watch:component']);
+
+gulp.task('watch:component', function() {
+	gulp.watch(['public/**', '!public/components/com_cars/assets/**'],
+		['simplecopy']);
+});
+
+gulp.task('watch:react', function() {
+	gulp.watch(['public/components/com_cars/assets/**', '!public/components/com_cars/assets/build', '!public/components/com_cars/assets/build/**'],
+		['copy']);
+});
+
+gulp.task('copy', ['webpack', 'clean'], copyToJoomla);
+gulp.task('simplecopy', ['clean'], copyToJoomla);
+
+gulp.task('clean', function(){
+	return del([config.joomlaDir + '/administrator/components/com_cars', config.joomlaDir + '/components/com_cars'], {force : true});
+});
